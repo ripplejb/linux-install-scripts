2ceae517c0d827980e2bc9d0efbd34bfcca2aef9
Ripal Barot <ripplejb@hotmail.com>
2026-05-20 22:37:51 -0500

    Added printer setup

---
+++ printer-setup/printer-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Add CUPS
+sudo pacman -Syu cups cups-browsed cups-filters cups-pdf system-config-printer --needed
+
+# Enable Services
+sudo systemctl enable --now cups.socket
+sudo systemctl enable --now cups-browsed.service
+sudo systemctl enable --now avahi-daemon.service
+
+# Firewall
+echo "If error mention no firewall, then ignore the message."
+sudo firewall-cmd --zone=public --add-service=mdns --permanent && sudo firewall-cmd --reload
